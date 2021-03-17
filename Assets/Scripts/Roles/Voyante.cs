3b57db90eac2315f7f1fb2b8a9f4e0727ba42a02
Grouxx <antoine.groux@gadz.org>
2021-03-17 16:28:19 +0100

    Create Groux adn multiple scripts

---
+++ Assets/Scripts/Roles/Voyante.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voyante : Player
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Player_Special_Vote(Player player, Voyante voyante)
+    {
+        // script qui désigne le joueur dont la voyante va dévoiler le rôle
+        if (voyante.Voting_Countdown == 0)
+        {
+            player.Role_Visible = true; 
+        }
+    }
+}
---
+++ Assets/Scripts/Roles/sorciere.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sorciere : Player
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Player_Special_Vote()
+    { 
+    // sript qui désigne la potion choisi par la sorcière et la personne sur laquelle elle l'utilise
+    }
+}
