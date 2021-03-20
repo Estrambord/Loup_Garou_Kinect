9a6a0df5a2bee4b1c6cd751c896afa19a90d0f64
Guillaume Martinez <73101383+Estrambord@users.noreply.github.com>
2021-03-20 19:44:19 +0100

    worked on the GameManager and created a simple fps counter prefab to add to any new scene

--- Assets/KinectAssets/KinectScripts/AvatarController.cs
+++ Assets/KinectAssets/KinectScripts/AvatarController.cs
@@ -574,20 +574,26 @@ public class AvatarController : MonoBehaviour
 	//	}
 	#endregion
 
-	#region Methods I added to the source AvatarController in order to check for gestures
+	#region Methods and Variables I added to the source AvatarController
 	public bool IsLeftHandUp { get; set; } = false;
 
 	public bool IsRightHandUp { get; set; } = false;
 
 	public bool AreBothHandsUp { get; set; } = false;
-	#endregion
 
-	#region Dictionaries
-	// dictionaries to speed up bones' processing
-	// the author of the terrific idea for kinect-joints to mecanim-bones mapping
-	// along with its initial implementation, including following dictionary is
-	// Mikhail Korchun (korchoon@gmail.com). Big thanks to this guy!
-	private readonly Dictionary<int, HumanBodyBones> boneIndex2MecanimMap = new Dictionary<int, HumanBodyBones>
+	public bool IsAvatarReady { get; set; } = false;
+
+	public string Role { get; set; } = "villageois";
+
+	public bool IsCapitaine { get; set; } = false;
+    #endregion
+
+    #region Dictionaries
+    // dictionaries to speed up bones' processing
+    // the author of the terrific idea for kinect-joints to mecanim-bones mapping
+    // along with its initial implementation, including following dictionary is
+    // Mikhail Korchun (korchoon@gmail.com). Big thanks to this guy!
+    private readonly Dictionary<int, HumanBodyBones> boneIndex2MecanimMap = new Dictionary<int, HumanBodyBones>
 	{
 		{0, HumanBodyBones.Hips},
 		{1, HumanBodyBones.Spine},
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,83 +3,242 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Game_Manager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    #region Variables
+
     private List<Player> Players;
     private float timer;
     private Canvas timer_UI;
     public GameObject Loup_Garou1;
     public LoupGarou loup2;
-    public AvatarController[] avatars;
+    public List<AvatarController> playersList;
     public KinectManager[] kinectManagers;
-    // Start is called before the first frame update
+
+    private int nbPlayersAlive;
+    private int nbLoupGarouAlive;
+
+    private bool beforeGameStart = true;
+    private bool jour = true;
+    private bool rolesSet = false;
+    private bool rolesDiscovered = false;
+    private bool capitaineElected = false;
+
+    public AvatarController playerKilled;
+
+    private bool gameOver = false;
+
+    #endregion
+
     void Start()
     {
-        
+        playersList = new List<AvatarController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (beforeGameStart) //initialisation du jeu, avant la premiere nuit
+        {
+            //Son d'introduction
+            //Son qui dit aux joueurs de se mettre à leur place et de lever les bras pour Ready
+            while (ArePlayersReady() == false)
+            {
+                //Afficher à l'écran que les joueurs ne sont pas prêts
+            }
+            if (rolesSet == false)
+            {
+                SetPlayersRoles();
+            }
+            if (rolesDiscovered == false)
+            {
+                //Son qui lance la nuit pour les joueurs
+                for (int i = 0; i < playersList.Count; i++)
+                {
+                    DiscoverOwnRole(playersList[i]);
+                }
+                //Son qui explique le but du jeu
+                //Son qui dit que tout le monde peut relever son masque
+                rolesDiscovered = true;
+            }
+            if (!capitaineElected)
+            {
+                //Son election du maire
+                ElectionCapitaine();
+            }
+
+        }
+
+        /*
+        else if (tourDeChauffe)
+        {
+            EST CE QU'ON FAIT UN TOUR POUR RIEN ? SYLVAIN AVAIT PROPOSE MAIS JE TROUVE QUE CA FERAIT PERDRE TROP DE TEMPS AUX JOUEURS
+        }
+        */
+
+        else if (!jour) //nuit
+        {
+            //Son pour que tout le monde mette son masque sur ses yeux
+            TourVoyante();
+            TourLoupGarou();
+            TourSorciere();
+            jour = true;
+        }
+        else if (jour) //jour
+        {
+            //Son pour que tout le monde enleve son masque des yeux
+
+            //Annonce des morts (fonction ? plusieurs ifs a la suite ?)
+
+            if (nbPlayersAlive <= 2*nbLoupGarouAlive)
+            {
+                gameOver = true;
+            }
+
+            if (!gameOver)
+            {
+                //Son debut du vote
+                VoteVillage();
+                //Son fin du vote et elimination d'un joueur
+            }
+
+            if (playerKilled.Role == "chasseur")
+            {
+                TourChasseur();
+            }
+            else if (playerKilled.IsCapitaine)
+            {
+                NouveauCapitaine();
+            }
+            jour = false;
+        }
+
+        if (gameOver)
+        {
+            if (nbLoupGarouAlive == 0)
+            {
+                //Son Bonne fin
+            }
+            else
+            {
+                //Son mauvaise fin
+            }
+        }
     }
 
-    public void Get_Ready()
-    { 
-    // script qui indique que le jeu commence lorsque tous les joueurs sont prêts et détecter par le Kinect
+
+    #region Methodes generales
+
+    /// <summary>
+    /// Indique si tous les joueurs sont detectes par la Kinect et prets
+    /// </summary>
+    public bool ArePlayersReady()
+    {
+        for (int i = 0; i < playersList.Count; i++)
+        {
+            if (!playersList[i].IsAvatarReady)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public void Discover_roles()
+    /// <summary>
+    /// Attribue un role a chaque joueur aleatoirement
+    /// </summary>
+    public void SetPlayersRoles()
     {
-        // script qui permet aux joueurs chacun leur tour de découvrir leur rôle
+        //Affecter les roles aux joueurs
+        rolesSet = true;
     }
 
-    public void Elect_Mayor()
+    /// <summary>
+    /// Permet à un joueur donne de decouvrir son role
+    /// </summary>
+    public void DiscoverOwnRole(AvatarController player)
     {
-        // script qui permet de faire un premier vote pour élire le maire
+        //Son qui demande à un joueur specifique d'enlever son masque
+
+        //Afficher le rôle du joueur
+
+        //Son qui lui demande d'interagir avec son role pour passer à la suite
+
+        //Son qui lui demande de remettre son masque
     }
 
-    public void Tour_Voyante()
+    /// <summary>
+    /// Lance l'election du maire
+    /// </summary>
+    public void ElectionCapitaine()
     {
-        // script qui gère le tour de la voyante
+
     }
 
-    public void Tour_Loup_Garou()
+    /// <summary>
+    /// Déclenche un vote d'elimination
+    /// </summary>
+    public void VoteVillage()
     {
-        // script qui gère le tour des Loups-Garous
+
     }
-    public void Tour_Sorciere() 
+    #endregion
+
+    #region Methodes specifiques a un joueur
+    /// <summary>
+    /// Lance les actions de la Voyante
+    /// </summary>
+    /// 
+    public void TourVoyante()
     {
-        // script qui gère le tour de la sorcière
+
     }
-    public void Tour_Chasseur()
+
+    /// <summary>
+    /// Lance le tour des Loups Garous
+    /// </summary>
+    public void TourLoupGarou()
     {
-        // script qui gère le tour du chasseur
+
     }
-    public void Vote_village() 
+
+    /// <summary>
+    /// Lance le tour de la Sorciere
+    /// </summary>
+    public void TourSorciere() 
     {
-        // script qui gère le vote quotidien
+
     }
-    public void Nuit() 
+
+    /// <summary>
+    /// Lance le tour du Chasseur
+    /// </summary>
+    public void TourChasseur()
     {
-        // script qui lance la nuit
+
     }
-    public void jour() 
+
+    public void NouveauCapitaine()
     {
-        // script qui lance la journée
+
     }
-    public void Display_UI() { }
+    #endregion
+
+
+    #region Methodes que je sais pas si elles seront utiles ou pas
+    public void DisplayUI() { }
+
     //public Player Get_Vote_Result(int Nb_Vote) { 
     // script qui gère le résultat du vote quotidien
     //}
+
     public void Execution() 
     {
     // script qui gère la mort d'un player
     }
-    public void Set_Players_Roles() 
-    {
-        // script qui attribue les rôles aux players
-    }
+    
+    
+
+
     public void Victory() 
     {
         // script qui donne le vainqueur de la partie
@@ -88,4 +247,5 @@ public class Game_Manager : MonoBehaviour
     {
         // script qui reload la partie si il y a un problème de tracking
     }
+    #endregion
 }
