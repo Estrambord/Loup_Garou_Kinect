97155b874dceae7e4878efad95ac6a4714bb0c44
Estrambord <guillaume.martinez.98@gmail.com>
2021-03-23 16:28:09 +0100

    prototype should be working 2

---
+++ Assets/Scripts/NbPlayersAlive.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NbPlayersAlive : MonoBehaviour
+{
+    public GameManager_Guillaume GM;
+    public Text nbPlayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nbPlayers.text = "NbPlayersAlive : " + GM.nbPlayersAlive.ToString();
+    }
+}
